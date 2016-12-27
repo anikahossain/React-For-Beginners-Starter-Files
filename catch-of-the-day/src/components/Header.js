bb924ee19f8b11049dbeafa1fad5464a849a00f5
Fariha Faruque-Kazi <anikahossain>
2016-12-27 14:55:59 -0500

    vid 8

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render() {
-    return (
+  const Header = (props) => {
+        return (
       <header className="top">
       <h1>
       Catch
@@ -12,11 +11,13 @@ class Header extends React.Component {
       </span>
       Day
       </h1>
-      <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      <h3 className="tagline"><span>{props.tagline}</span></h3>
       </header>
       )
   }
-}
+
+
+
 
 export default Header;
 
