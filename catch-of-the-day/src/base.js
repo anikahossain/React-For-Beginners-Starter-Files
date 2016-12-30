4316d7469b86303738004e903637a2d2edf9744e
Fariha Faruque-Kazi <anikahossain>
2016-12-30 15:00:27 -0500

    vid 18 finally -___-

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAC0I2Zd_A8HBuGeEuueitTOLnP5eRiteI",
+    authDomain: "catch-of-the-day-fariha-kazi.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-fariha-kazi.firebaseio.com",
+});
+
+export default base;
