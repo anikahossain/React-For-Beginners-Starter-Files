ef2ad0647696b48588ad5afaae0863fce86bcb2a
Fariha Faruque-Kazi <anikahossain>
2016-12-30 13:45:16 -0500

    vid 17

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
       //update or add the new number of fish ordered
       order[key] = order[key] + 1 || 1;
       //update our state
-      this.setState({ order })
+      this.setState({ order });
     }
 
 
@@ -54,11 +54,11 @@ class App extends React.Component {
       {
         Object
         .keys(this.state.fishes)
-        .map(key => <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+        .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
       }
       </ul>
       </div>
-      <Order />
+      <Order fishes={this.state.fishes} order={this.state.order}/>
       <Inventory addFish={this.addFish}  loadSamples={this.loadSamples} />
       </div>
       )
